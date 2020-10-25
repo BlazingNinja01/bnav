c45e9ec9e4f5ca17b6ba26d6e24a6dfa5cba8fc7
Hassan Shahid <hassanshahidofficial@gmail.com>
2020-10-26 01:22:44 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bnav/app.dart';
+import 'package:bnav/bottom_navigation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: App(),
+    );
+  }
+}
---
+++ lib/pages.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Page1a()));
+              },
+              color: Colors.blue,
+              child: Text('page 1a'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatefulWidget {
+  @override
+  _Page2State createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            MaterialButton(
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => Page2a()));
+              },
+              color: Colors.red,
+              child: Text('page 2a'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatefulWidget {
+  @override
+  _Page3State createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('page 3'),
+      ),
+    );
+  }
+}
+
+class Page1a extends StatefulWidget {
+  @override
+  _Page1aState createState() => _Page1aState();
+}
+
+class _Page1aState extends State<Page1a> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Text('hello'),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Page1b()));
+              },
+              color: Colors.green,
+              child: Text('page 1b'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page1b extends StatefulWidget {
+  @override
+  _Page1bState createState() => _Page1bState();
+}
+
+class _Page1bState extends State<Page1b> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('page 1b'),
+      ),
+    );
+  }
+}
+
+
+class Page2a extends StatefulWidget {
+  @override
+  _Page2aState createState() => _Page2aState();
+}
+
+class _Page2aState extends State<Page2a> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('page 2a'),
+      ),
+    );
+}
+}
